9ecf7a35bc19cff841ba623df391d4abdfeaea36
diskree <diskriminant.was.taken@gmail.com>
2022-11-08 21:13:52 +0400

    0.1 Fix locked food with overriden finishUsing()

---
+++ src/main/java/com/diskree/achievetodo/mixin/SuspiciousStewItemMixin.java
@@ -0,0 +1,26 @@
+package com.diskree.achievetodo.mixin;
+
+import com.diskree.achievetodo.AchieveToDoMod;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SuspiciousStewItem;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SuspiciousStewItem.class)
+public class SuspiciousStewItemMixin {
+
+    @Inject(method = "finishUsing", at = @At("HEAD"), cancellable = true)
+    private void finishUsingInject(ItemStack stack, World world, LivingEntity user, CallbackInfoReturnable<ItemStack> cir) {
+        if (stack != null && stack.getItem() != null && stack.getItem().isFood() && user instanceof PlayerEntity && AchieveToDoMod.isFoodBlocked(stack.getItem().getFoodComponent())) {
+            MinecraftClient.getInstance().options.useKey.setPressed(false);
+            AchieveToDoMod.showFoodBlockedDescription(stack.getItem().getFoodComponent());
+            cir.setReturnValue(stack);
+        }
+    }
+}

f0dd6c34c045bdfe7e04b4cc8a9e4b5ff1100d9a
diskree <diskriminant.was.taken@gmail.com>
2022-11-26 18:25:38 +0400

    Water bucket lock also applies to buckets with entity

--- src/main/java/com/diskree/achievetodo/mixin/PlayerEntityMixin.java
+++ src/main/java/com/diskree/achievetodo/mixin/PlayerEntityMixin.java
@@ -3,6 +3,7 @@ package com.diskree.achievetodo.mixin;
 import com.diskree.achievetodo.AchieveToDoMod;
 import com.diskree.achievetodo.BlockedAction;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.EntityBucketItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.math.BlockPos;
@@ -25,7 +26,7 @@ public abstract class PlayerEntityMixin {
 
     @Inject(method = "canPlaceOn", at = @At("HEAD"), cancellable = true)
     public void canPlaceOnInject(BlockPos pos, Direction facing, ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if (stack != null && stack.isOf(Items.WATER_BUCKET) && AchieveToDoMod.isActionBlocked(BlockedAction.using_water_bucket)) {
+        if (stack != null && (stack.isOf(Items.WATER_BUCKET) || stack.getItem() instanceof EntityBucketItem) && AchieveToDoMod.isActionBlocked(BlockedAction.using_water_bucket)) {
             cir.setReturnValue(false);
         }
     }

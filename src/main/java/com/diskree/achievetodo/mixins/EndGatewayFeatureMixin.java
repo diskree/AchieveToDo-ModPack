e642ef2e66afe5f3c4308979c1c2b87c612caa8a
diskree <sergei.grushin@imem.app>
2023-05-23 10:43:08 +0400

    Dragon egg. Moved from center end portal to every end gate roof

---
+++ src/main/java/com/diskree/achievetodo/mixins/EndGatewayFeatureMixin.java
@@ -0,0 +1,19 @@
+package com.diskree.achievetodo.mixins;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.world.gen.feature.EndGatewayFeature;
+import net.minecraft.world.gen.feature.EndGatewayFeatureConfig;
+import net.minecraft.world.gen.feature.util.FeatureContext;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(EndGatewayFeature.class)
+public class EndGatewayFeatureMixin {
+
+    @Inject(method = "generate", at = @At("TAIL"))
+    public void generateInject(FeatureContext<EndGatewayFeatureConfig> context, CallbackInfoReturnable<Boolean> cir) {
+        ((EndGatewayFeature) (Object) this).setBlockState(context.getWorld(), context.getOrigin().up(3), Blocks.DRAGON_EGG.getDefaultState());
+    }
+}
---
+++ src/main/java/com/diskree/achievetodo/mixins/EnderDragonFightMixin.java
@@ -0,0 +1,19 @@
+package com.diskree.achievetodo.mixins;
+
+import net.minecraft.entity.boss.dragon.EnderDragonFight;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(EnderDragonFight.class)
+public class EnderDragonFightMixin {
+
+    @Shadow private boolean previouslyKilled;
+
+    @Inject(method = "generateNewEndGateway", at = @At("HEAD"))
+    public void generateNewEndGatewayInject(CallbackInfo ci) {
+        previouslyKilled = true;
+    }
+}

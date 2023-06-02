6d45f02042dd10ee9ef8d917a8e48808b56c5f10
diskree <sergei.grushin@imem.app>
2023-06-02 07:08:25 +0400

    Auto-Lock difficulty

---
+++ src/main/java/com/diskree/achievetodo/mixins/LockButtonWidgetMixin.java
@@ -0,0 +1,20 @@
+package com.diskree.achievetodo.mixins;
+
+import net.minecraft.client.gui.widget.LockButtonWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LockButtonWidget.class)
+public class LockButtonWidgetMixin {
+
+    @Shadow
+    private boolean locked;
+
+    @Inject(method = "setLocked", at = @At("RETURN"))
+    public void setLockedInject(boolean locked, CallbackInfo ci) {
+        this.locked = true;
+    }
+}

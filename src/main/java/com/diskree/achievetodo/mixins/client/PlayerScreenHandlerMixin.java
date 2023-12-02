5ebf9d860ddac2b676c793cbcb8aedcc8f406669
diskree <diskreee@gmail.com>
2023-12-02 21:09:19 +0400

    Fix armor lock hack in inventory

---
+++ src/main/java/com/diskree/achievetodo/mixins/client/PlayerScreenHandlerMixin.java
@@ -0,0 +1,59 @@
+package com.diskree.achievetodo.mixins.client;
+
+import com.diskree.achievetodo.AchieveToDo;
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(PlayerScreenHandler.class)
+public abstract class PlayerScreenHandlerMixin {
+
+    @ModifyArg(method = "<init>",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/screen/PlayerScreenHandler;addSlot(Lnet/minecraft/screen/slot/Slot;)Lnet/minecraft/screen/slot/Slot;",
+                    ordinal = 2
+            )
+    )
+    private Slot initInject(Slot slot) {
+        return new Slot(slot.inventory, slot.getIndex(), slot.x, slot.y) {
+            @Override
+            public void setStackByPlayer(ItemStack stack) {
+                slot.setStackByPlayer(stack);
+            }
+
+            @Override
+            public int getMaxItemCount() {
+                return slot.getMaxItemCount();
+            }
+
+            @Override
+            public boolean canInsert(ItemStack stack) {
+                if (stack != null && AchieveToDo.isEquipmentBlocked(MinecraftClient.getInstance().player, stack.getItem())) {
+                    if (MinecraftClient.getInstance().currentScreen != null) {
+                        MinecraftClient.getInstance().currentScreen.closeScreen();
+                    }
+                    return false;
+                }
+                return slot.canInsert(stack);
+            }
+
+            @Override
+            public boolean canTakeItems(PlayerEntity playerEntity) {
+                return slot.canTakeItems(playerEntity);
+            }
+
+            @Override
+            public Pair<Identifier, Identifier> getBackgroundSprite() {
+                return slot.getBackgroundSprite();
+            }
+        };
+    }
+}

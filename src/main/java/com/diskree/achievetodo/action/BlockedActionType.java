02d1144c59a6934b4d27836b325c0d124998f04b
diskree <diskreee@gmail.com>
2024-01-05 23:38:18 +0400

    New blocked actions

--- src/main/java/com/diskree/achievetodo/action/BlockedActionType.java
+++ src/main/java/com/diskree/achievetodo/action/BlockedActionType.java
@@ -23,161 +23,290 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum BlockedActionType implements IGeneratedAdvancement {
-    EAT_SALMON(
-            2, FoodComponents.SALMON
-    ),
-    EAT_COD(
-            3, FoodComponents.COD
-    ),
-    EAT_TROPICAL_FISH(
-            4, FoodComponents.TROPICAL_FISH
-    ),
-    EAT_ROTTEN_FLESH(
-            6, FoodComponents.ROTTEN_FLESH
-    ),
     JUMP(
-            7
+            8
     ),
-    EAT_SPIDER_EYE(
-            8, FoodComponents.SPIDER_EYE
+    USING_DOOR(
+            15
     ),
-    EAT_PUFFERFISH(
-            12, FoodComponents.PUFFERFISH
+    SLEEP(
+            20
     ),
-    EAT_CHICKEN(
-            15, FoodComponents.CHICKEN
-    ),
-    EAT_POISONOUS_POTATO(
-            17, FoodComponents.POISONOUS_POTATO
-    ),
-    EAT_SUSPICIOUS_STEW(
-            20, FoodComponents.SUSPICIOUS_STEW
-    ),
-    EAT_MUTTON(
-            22, FoodComponents.MUTTON
-    ),
-    EAT_RABBIT(
-            24, FoodComponents.RABBIT
-    ),
-    BREAK_BLOCKS_IN_POSITIVE_Y(
+    OPEN_INVENTORY(
             25
     ),
-    EAT_SWEET_BERRIES(
-            27, FoodComponents.SWEET_BERRIES
-    ),
-    EAT_GLOW_BERRIES(
-            29, FoodComponents.GLOW_BERRIES
-    ),
-    EAT_PORKCHOP(
-            33, FoodComponents.PORKCHOP
-    ),
-    EAT_BEETROOT(
-            37, FoodComponents.BEETROOT
-    ),
-    EAT_BEETROOT_SOUP(
-            45, FoodComponents.BEETROOT_SOUP
-    ),
-    USING_CRAFTING_TABLE(
-            50, Blocks.CRAFTING_TABLE
-    ),
-    EAT_CARROT(
-            52, FoodComponents.CARROT
-    ),
-    EAT_APPLE(
-            55, FoodComponents.APPLE
-    ),
-    EAT_POTATO(
-            58, FoodComponents.POTATO
-    ),
-    USING_FURNACE(
-            60, Blocks.FURNACE
-    ),
-    EAT_DRIED_KELP(
-            62, FoodComponents.DRIED_KELP
-    ),
-    EAT_MELON_SLICE(
-            64, FoodComponents.MELON_SLICE
-    ),
-    USING_STONE_TOOLS(
-            70, ToolMaterials.STONE
-    ),
-    EAT_COOKIE(
-            72, FoodComponents.COOKIE
-    ),
-    EAT_HONEY_BOTTLE(
-            77, FoodComponents.HONEY_BOTTLE
+    BREAK_BLOCKS_IN_POSITIVE_Y(
+            30
     ),
     USING_BOAT(
             80
-    ),
-    EAT_BEEF(
-            88, FoodComponents.BEEF
     ),
     USING_SHIELD(
             90
     ),
     USING_WATER_BUCKET(
-            95
+            130
     ),
-    EAT_BREAD(
-            100, FoodComponents.BREAD
+    USING_SHEARS(
+            140
     ),
-    EAT_RABBIT_STEW(
-            105, FoodComponents.RABBIT_STEW
-    ),
-    USING_IRON_TOOLS(
-            110, ToolMaterials.IRON
-    ),
-    EAT_MUSHROOM_STEW(
-            111, FoodComponents.MUSHROOM_STEW
-    ),
-    EAT_BAKED_POTATO(
-            130, FoodComponents.BAKED_POTATO
-    ),
-    EAT_PUMPKIN_PIE(
-            145, FoodComponents.PUMPKIN_PIE
-    ),
-    EQUIP_IRON_ARMOR(
-            150, ArmorMaterials.IRON
-    ),
-    USING_ANVIL(
-            175, Blocks.ANVIL
-    ),
-    EAT_COOKED_SALMON(
-            190, FoodComponents.COOKED_SALMON
-    ),
-    EAT_COOKED_COD(
-            195, FoodComponents.COOKED_COD
+    USING_CROSSBOW(
+            150
     ),
     BREAK_BLOCKS_IN_NEGATIVE_Y(
             200
     ),
-    VILLAGER_MASON(
-            210, VillagerProfession.MASON
-    ),
-    EAT_COOKED_RABBIT(
-            220, FoodComponents.COOKED_RABBIT
-    ),
-    VILLAGER_CARTOGRAPHER(
-            230, VillagerProfession.CARTOGRAPHER
+    USING_FISHING_ROD(
+            220
     ),
     USING_BOW(
             240
     ),
-    USING_SMOKER(
-            250, Blocks.SMOKER
+    USING_BRUSH(
+            260
+    ),
+    USING_TRIDENT(
+            300
+    ),
+    USING_SPYGLASS(
+            280
+    ),
+    EQUIP_ELYTRA(
+            650
+    ),
+    END_GATEWAY(
+            675
+    ),
+    USING_FIREWORKS_WHILE_FLY(
+            725
+    ),
+    USING_SHULKER_BOX(
+            750
+    ),
+
+    EAT_SALMON(
+            3, FoodComponents.SALMON
+    ),
+    EAT_COD(
+            4, FoodComponents.COD
+    ),
+    EAT_TROPICAL_FISH(
+            5, FoodComponents.TROPICAL_FISH
+    ),
+    EAT_ROTTEN_FLESH(
+            10, FoodComponents.ROTTEN_FLESH
+    ),
+    EAT_SPIDER_EYE(
+            12, FoodComponents.SPIDER_EYE
+    ),
+    EAT_SWEET_BERRIES(
+            14, FoodComponents.SWEET_BERRIES
+    ),
+    EAT_GLOW_BERRIES(
+            16, FoodComponents.GLOW_BERRIES
+    ),
+    EAT_PUFFERFISH(
+            18, FoodComponents.PUFFERFISH
+    ),
+    EAT_POISONOUS_POTATO(
+            20, FoodComponents.POISONOUS_POTATO
+    ),
+    EAT_CHORUS_FRUIT(
+            25, FoodComponents.CHORUS_FRUIT
+    ),
+    EAT_SUSPICIOUS_STEW(
+            30, FoodComponents.SUSPICIOUS_STEW
+    ),
+    EAT_BEETROOT(
+            35, FoodComponents.BEETROOT
+    ),
+    EAT_CARROT(
+            40, FoodComponents.CARROT
+    ),
+    EAT_CHICKEN(
+            45, FoodComponents.CHICKEN
+    ),
+    EAT_DRIED_KELP(
+            50, FoodComponents.DRIED_KELP
+    ),
+    EAT_BEETROOT_SOUP(
+            60, FoodComponents.BEETROOT_SOUP
+    ),
+    EAT_POTATO(
+            70, FoodComponents.POTATO
+    ),
+    EAT_APPLE(
+            80, FoodComponents.APPLE
+    ),
+    EAT_MELON_SLICE(
+            90, FoodComponents.MELON_SLICE
+    ),
+    EAT_COOKIE(
+            100, FoodComponents.COOKIE
+    ),
+    EAT_MUSHROOM_STEW(
+            110, FoodComponents.MUSHROOM_STEW
+    ),
+    EAT_RABBIT_STEW(
+            120, FoodComponents.RABBIT_STEW
+    ),
+    EAT_HONEY_BOTTLE(
+            130, FoodComponents.HONEY_BOTTLE
+    ),
+    EAT_PUMPKIN_PIE(
+            140, FoodComponents.PUMPKIN_PIE
     ),
     EAT_GOLDEN_APPLE(
-            260, FoodComponents.GOLDEN_APPLE
+            150, FoodComponents.GOLDEN_APPLE
+    ),
+    EAT_ENCHANTED_GOLDEN_APPLE(
+            160, FoodComponents.ENCHANTED_GOLDEN_APPLE
+    ),
+    EAT_BAKED_POTATO(
+            180, FoodComponents.BAKED_POTATO
+    ),
+    EAT_RABBIT(
+            200, FoodComponents.RABBIT
+    ),
+    EAT_MUTTON(
+            220, FoodComponents.MUTTON
+    ),
+    EAT_PORKCHOP(
+            240, FoodComponents.PORKCHOP
+    ),
+    EAT_BEEF(
+            250, FoodComponents.BEEF
+    ),
+    EAT_COOKED_SALMON(
+            300, FoodComponents.COOKED_SALMON
+    ),
+    EAT_COOKED_COD(
+            400, FoodComponents.COOKED_COD
+    ),
+    EAT_COOKED_RABBIT(
+            450, FoodComponents.COOKED_RABBIT
+    ),
+    EAT_COOKED_CHICKEN(
+            500, FoodComponents.COOKED_CHICKEN
+    ),
+    EAT_COOKED_MUTTON(
+            550, FoodComponents.COOKED_MUTTON
+    ),
+    EAT_COOKED_PORKCHOP(
+            600, FoodComponents.COOKED_PORKCHOP
+    ),
+    EAT_COOKED_BEEF(
+            650, FoodComponents.COOKED_BEEF
+    ),
+    EAT_BREAD(
+            700, FoodComponents.BREAD
+    ),
+    EAT_GOLDEN_CARROT(
+            800, FoodComponents.GOLDEN_CARROT
+    ),
+
+    USING_CHEST(
+            30, Blocks.CHEST
+    ),
+    USING_CRAFTING_TABLE(
+            50, Blocks.CRAFTING_TABLE
+    ),
+    USING_STONECUTTER(
+            75, Blocks.STONECUTTER
+    ),
+    USING_FURNACE(
+            100, Blocks.FURNACE
+    ),
+    USING_ANVIL(
+            150, Blocks.ANVIL
+    ),
+    USING_GRINDSTONE(
+            175, Blocks.GRINDSTONE
+    ),
+    USING_LOOM(
+            200, Blocks.LOOM
+    ),
+    USING_SMOKER(
+            250, Blocks.SMOKER
     ),
     USING_BLAST_FURNACE(
             270, Blocks.BLAST_FURNACE
     ),
-    VILLAGER_LEATHERWORKER(
-            280, VillagerProfession.LEATHERWORKER
+    USING_CARTOGRAPHY_TABLE(
+            300, Blocks.CARTOGRAPHY_TABLE
     ),
+    USING_ENDER_CHEST(
+            350, Blocks.ENDER_CHEST
+    ),
+    USING_BREWING_STAND(
+            400, Blocks.BREWING_STAND
+    ),
+    USING_SMITHING_TABLE(
+            450, Blocks.SMITHING_TABLE
+    ),
+    USING_BEACON(
+            500, Blocks.BEACON
+    ),
+    USING_ENCHANTING_TABLE(
+            950, Blocks.ENCHANTING_TABLE
+    ),
+
+    USING_GOLD_TOOLS(
+            40, ToolMaterials.GOLD
+    ),
+    USING_WOOD_TOOLS(
+            70, ToolMaterials.WOOD
+    ),
+    USING_STONE_TOOLS(
+            90, ToolMaterials.STONE
+    ),
+    USING_IRON_TOOLS(
+            120, ToolMaterials.IRON
+    ),
+    USING_DIAMOND_TOOLS(
+            350, ToolMaterials.DIAMOND
+    ),
+    USING_NETHERITE_TOOLS(
+            500, ToolMaterials.NETHERITE
+    ),
+
+    EQUIP_GOLD_ARMOR(
+            40, ArmorMaterials.GOLD
+    ),
+    EQUIP_LEATHER_ARMOR(
+            90, ArmorMaterials.LEATHER
+    ),
+    EQUIP_IRON_ARMOR(
+            150, ArmorMaterials.IRON
+    ),
+    EQUIP_CHAIN_ARMOR(
+            200, ArmorMaterials.CHAIN
+    ),
+    EQUIP_TURTLE_ARMOR(
+            300, ArmorMaterials.TURTLE
+    ),
+    EQUIP_DIAMOND_ARMOR(
+            400, ArmorMaterials.DIAMOND
+    ),
+    EQUIP_NETHERITE_ARMOR(
+            550, ArmorMaterials.NETHERITE
+    ),
+
     NETHER(
             300, World.NETHER
+    ),
+    END(
+            600, World.END
+    ),
+
+    VILLAGER_MASON(
+            210, VillagerProfession.MASON
+    ),
+    VILLAGER_CARTOGRAPHER(
+            230, VillagerProfession.CARTOGRAPHER
+    ),
+    VILLAGER_LEATHERWORKER(
+            280, VillagerProfession.LEATHERWORKER
     ),
     VILLAGER_SHEPHERD(
             315, VillagerProfession.SHEPHERD
@@ -185,89 +314,29 @@ public enum BlockedActionType implements IGeneratedAdvancement {
     VILLAGER_BUTCHER(
             330, VillagerProfession.BUTCHER
     ),
-    USING_DIAMOND_TOOLS(
-            350, ToolMaterials.DIAMOND
-    ),
-    EQUIP_DIAMOND_ARMOR(
-            380, ArmorMaterials.DIAMOND
-    ),
     VILLAGER_FARMER(
             385, VillagerProfession.FARMER
-    ),
-    USING_ENDER_CHEST(
-            390, Blocks.ENDER_CHEST
     ),
     VILLAGER_CLERIC(
             400, VillagerProfession.CLERIC
     ),
-    USING_BREWING_STAND(
-            420, Blocks.BREWING_STAND
-    ),
     VILLAGER_FISHERMAN(
             444, VillagerProfession.FISHERMAN
-    ),
-    EAT_COOKED_CHICKEN(
-            450, FoodComponents.COOKED_CHICKEN
-    ),
-    EAT_COOKED_MUTTON(
-            480, FoodComponents.COOKED_MUTTON
-    ),
-    USING_BEACON(
-            500, Blocks.BEACON
-    ),
-    EAT_COOKED_PORKCHOP(
-            510, FoodComponents.COOKED_PORKCHOP
     ),
     VILLAGER_FLETCHER(
             520, VillagerProfession.FLETCHER
     ),
-    USING_NETHERITE_TOOLS(
-            530, ToolMaterials.NETHERITE
-    ),
     VILLAGER_ARMORER(
             540, VillagerProfession.ARMORER
-    ),
-    EQUIP_NETHERITE_ARMOR(
-            550, ArmorMaterials.NETHERITE
-    ),
-    EAT_COOKED_BEEF(
-            580, FoodComponents.COOKED_BEEF
     ),
     VILLAGER_WEAPONSMITH(
             590, VillagerProfession.WEAPONSMITH
     ),
-    END(
-            600, World.END
-    ),
-    EQUIP_ELYTRA(
-            650
-    ),
-    EAT_ENCHANTED_GOLDEN_APPLE(
-            680, FoodComponents.ENCHANTED_GOLDEN_APPLE
-    ),
-    EAT_CHORUS_FRUIT(
-            700, FoodComponents.CHORUS_FRUIT
-    ),
-    END_GATEWAY(
-            725
-    ),
-    USING_FIREWORKS_WHILE_FLY(
-            750
-    ),
-    USING_SHULKER_BOX(
-            825
-    ),
     VILLAGER_TOOLSMITH(
             860, VillagerProfession.TOOLSMITH
     ),
-    EAT_GOLDEN_CARROT(
-            888, FoodComponents.GOLDEN_CARROT
-    ),
     VILLAGER_LIBRARIAN(
             900, VillagerProfession.LIBRARIAN
-    ),
-    USING_ENCHANTING_TABLE(
-            950, Blocks.ENCHANTING_TABLE
     );
 
     private final int unblockAdvancementsCount;
